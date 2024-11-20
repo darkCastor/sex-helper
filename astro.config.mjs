ead9c42e8b7d42ca5644dd7d9c9708586233a3b6
Your Name <you@example.com>
2024-11-20 21:17:52 +0100

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    AstroPWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Your App Name",
+        short_name: "App",
+        description: "Your app description",
+        theme_color: "#13151a",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallback: "/",
+      },
+    }),
+  ],
+});
